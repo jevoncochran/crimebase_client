5576c3551e8c495c678a39c9822ae5d926ee2912
Jevon Cochran <jevon@securebags.com>
2023-09-06 16:36:23 -0500

    getLocalCases

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -62,6 +62,7 @@ interface Case {
 
 export default function Home() {
   const [buzzingCases, setBuzzingCases] = useState<Case[]>([]);
+  const [localCases, setLocalCases] = useState<Case[]>([]);
 
   useEffect(() => {
     const getBuzzingCases = () => {
@@ -74,25 +75,58 @@ export default function Home() {
           console.log(err);
         });
     };
+
+    const getLocalCases = () => {
+      axios
+        .get("http://localhost:8000/api/cases/local")
+        .then((res) => {
+          setLocalCases(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+
     getBuzzingCases();
+    getLocalCases();
   }, []);
   return (
     <main className={classes.main}>
-      <h2 className={classes.title}>In the News</h2>
-      <div className={classes.casesContainer}>
-        {buzzingCases.map((caseInfo) => (
-          <div key={caseInfo.id} className={classes.caseCard}>
-            <p className={classes.caseTitle}>{caseInfo.title}</p>
-            {caseInfo.mainImageUrl ? (
-              <Image
-                src={caseInfo.mainImageUrl}
-                alt={caseInfo.title}
-                height={400}
-                width={400}
-              />
-            ) : null}
-          </div>
-        ))}
+      <div className={classes.sectionWrapper}>
+        <h2 className={classes.title}>In the News</h2>
+        <div className={classes.casesContainer}>
+          {buzzingCases.map((caseInfo) => (
+            <div key={caseInfo.id} className={classes.caseCard}>
+              <p className={classes.caseTitle}>{caseInfo.title}</p>
+              {caseInfo.mainImageUrl ? (
+                <Image
+                  src={caseInfo.mainImageUrl}
+                  alt={caseInfo.title}
+                  height={400}
+                  width={400}
+                />
+              ) : null}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className={classes.sectionWrapper}>
+        <h2 className={classes.title}>Local Cases</h2>
+        <div className={classes.casesContainer}>
+          {localCases.map((caseInfo) => (
+            <div key={caseInfo.id} className={classes.caseCard}>
+              <p className={classes.caseTitle}>{caseInfo.title}</p>
+              {caseInfo.mainImageUrl ? (
+                <Image
+                  src={caseInfo.mainImageUrl}
+                  alt={caseInfo.title}
+                  height={400}
+                  width={400}
+                />
+              ) : null}
+            </div>
+          ))}
+        </div>
       </div>
     </main>
   );
